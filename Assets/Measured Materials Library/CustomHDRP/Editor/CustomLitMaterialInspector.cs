2871f40ab6ad88fb6e6d3e760c9870d6df5f61a2
RoiIam <robkokica@gmail.com>
2024-04-24 17:51:11 +0200

    Editing GUI, removed all but GlintsMethodUIBlock custom blocks and made it show all ExpandableBit. So it is not a mess, now it just complains that we have unknown _MaterialID

--- Assets/Measured Materials Library/CustomHDRP/Editor/CustomLitMaterialInspector.cs
+++ Assets/Measured Materials Library/CustomHDRP/Editor/CustomLitMaterialInspector.cs
@@ -1,17 +1,30 @@
 using UnityEditor.Rendering.HighDefinition;
-using UnityEngine;
-
+//little hack is used, we inherit from LightingShaderGraphGUI even though it should be LitGUI but that
+//was really messy, didnt work as intended. instead we just recreate MaterialUIBlockList
 // https://docs.unity3d.com/Packages/com.unity.render-pipelines.high-definition@11.0/manual/hdrp-custom-material-inspector.html
 public class CustomLitMaterialInspector : LightingShaderGraphGUI
 {
     public CustomLitMaterialInspector()
     {
         // Remove the ShaderGraphUIBlock to avoid having duplicated properties in the UI.
-        //uiBlocks.RemoveAll(b => b is ShaderGraphUIBlock);
-        
-        
-        //uiBlocks.Insert(1,new  TextDictUIBlock("myColor", "test zmeny baseColor v LitDataindividualLayer.hlsl"));//RCC gui text
-        uiBlocks.Insert(1, new TextDictUIBlock());//RCC gui text
-        uiBlocks.Insert(2, new GlintsMethodUIBlock());
+        uiBlocks.RemoveAll(b => b is ShaderGraphUIBlock);
+        // For lit GUI we don't display the heightmap nor layering options
+        const LitSurfaceInputsUIBlock.Features litSurfaceFeatures = LitSurfaceInputsUIBlock.Features.All ^
+                                                                    LitSurfaceInputsUIBlock.Features.HeightMap ^
+                                                                    LitSurfaceInputsUIBlock.Features.LayerOptions;
+        var uiBlocks2 = new MaterialUIBlockList
+        {
+            //new SurfaceOptionUIBlock(MaterialUIBlock.ExpandableBit.Base, features: SurfaceOptionUIBlock.Features.Lit),
+            new TessellationOptionsUIBlock(MaterialUIBlock.ExpandableBit.Tessellation),
+            new LitSurfaceInputsUIBlock(MaterialUIBlock.ExpandableBit.Input, features: litSurfaceFeatures),
+            new DetailInputsUIBlock(MaterialUIBlock.ExpandableBit.Detail),
+            // We don't want distortion in Lit
+            new TransparencyUIBlock(MaterialUIBlock.ExpandableBit.Transparency,
+                TransparencyUIBlock.Features.All & ~TransparencyUIBlock.Features.Distortion),
+            new EmissionUIBlock(MaterialUIBlock.ExpandableBit.Emissive)
+            //new AdvancedOptionsUIBlock(MaterialUIBlock.ExpandableBit.Advance, AdvancedOptionsUIBlock.Features.StandardLit),
+        };
+        uiBlocks.AddRange(uiBlocks2);
+        uiBlocks.Add(new GlintsMethodUIBlock(MaterialUIBlock.ExpandableBit.User0));
     }
 }
--- Assets/Measured Materials Library/CustomHDRP/Editor/GlintsMethodUIBlock.cs
+++ Assets/Measured Materials Library/CustomHDRP/Editor/GlintsMethodUIBlock.cs
@@ -1,29 +1,93 @@
-using UnityEditor.Rendering.HighDefinition;
 using UnityEditor;
-using UnityEngine;
+using UnityEditor.Rendering;
+using UnityEditor.Rendering.HighDefinition;
 
 // Include material common properties names to acces MaterialId
 //using static UnityEngine.Rendering.HighDefinition.HDMaterialProperties; // cannot acces internal MaterialId.LitSSS anyways....
 public class GlintsMethodUIBlock : MaterialUIBlock
 {
-    MaterialProperty glintsBlock;
-    MaterialProperty matID;
+    private readonly ExpandableBit foldoutBit;
 
-    
+    private MaterialProperty dictBlock;
+    private MaterialProperty glintsMethod;
+    private MaterialProperty matID;
+
+
+    private MaterialProperty maxNDFBlock;
+    private MaterialProperty targetNDFBlock;
+
+
+    public GlintsMethodUIBlock(ExpandableBit expandableBit)
+    {
+        foldoutBit = expandableBit;
+    }
+
     public override void LoadMaterialProperties()
     {
-        glintsBlock = FindProperty("_glintsMethod");
         matID = FindProperty("_MaterialID"); //big D...
+        glintsMethod = FindProperty("_glintsMethod");
 
+        dictBlock = FindProperty("_testDict");
+
+        maxNDFBlock = FindProperty("_maxNDF");
+        targetNDFBlock = FindProperty("_targetNDF");
         //Debug.Log(matID);
     }
 
+    public void ShowChermainParams()
+    {
+        materialEditor.ShaderProperty(dictBlock, "Test Dict");
+    }
+
+    public void ShowDeliotParams()
+    {
+        materialEditor.ShaderProperty(maxNDFBlock, "max NDF");
+        materialEditor.ShaderProperty(targetNDFBlock, "target NDF");
+    }
+
+    public void ShowZirrParams()
+    {
+    }
+
+    public void ShowWangParams()
+    {
+    }
+
+    public void ShowWangModParams()
+    {
+    }
+
+    //TODO remove magic numbers
     public override void OnGUI()
     {
-        if ((int)matID.floatValue == 6)  //if glints //be sure the enum is int doesnt work, nah cast this to int bcs its inconsistent in HDRP...
-        {
-            materialEditor.ShaderProperty(glintsBlock, "Glints Method");
+        if ((int)matID.floatValue == 6)
 
-        }
+
+            using (var header = new MaterialHeaderScope("Glints Options", (uint)foldoutBit, materialEditor))
+            {
+                //doesnt work for now...
+                //we should check here if its the DB23 method
+                //Debug.Log((int)matID.floatValue);
+
+                if (header.expanded)
+                    switch ((int)glintsMethod.floatValue)
+                    {
+                        case 1:
+                            ShowChermainParams();
+                            break;
+                        case 2:
+                            ShowDeliotParams();
+                            break;
+                        case 3:
+                            ShowZirrParams();
+                            break;
+                        case 4:
+                            ShowWangParams();
+                            break;
+                        case 5:
+                            ShowWangModParams();
+                            break;
+                    }
+            }
     }
 }
